69b6a7caed440bd5ef9c04f576e07a78665af3bb
ccastrom <claudiocastrom7@gmail.com>
2026-03-07 20:53:12 -0300

    feat(constants): add BAUTIZO_CANCELADO tramite status

--- src/constants/estados_tramites.js
+++ src/constants/estados_tramites.js
@@ -1,12 +1,13 @@
 
 const ESTADOS_VALIDOS={
-    INICIADO:'Iniciado',
+    Tramite_Iniciad:'Tramite iniciado',
     Reunion_Pre_Bautizo_Asignada:'Reunion Pre bautizo asignada',
     Reunion_Pre_Bautizo_Completada:'Reunion Pre bautizo completada',
     Evento_Bautizo_Agendado:'Evento bautizo agendado',
     Bautizo_Finalizado_Con_Éxito:'Bautizo finalizado con éxito',
     Bautizo_Derivado_A_Otra_Parroquia:'Bautizo derivado a otra parroquia',
-    Bautizo_Reabierto:'Bautizo reabierto'
+    Bautizo_Reabierto:'Bautizo reabierto',
+    Bautizo_Cancelado:'Bautizo cancelado'
 };
 
 module.exports = { ESTADOS_VALIDOS };
