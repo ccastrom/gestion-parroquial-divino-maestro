75003de930691642066324d9232a71e918f81abd
ccastrom <claudiocastrom7@gmail.com>
2026-03-08 23:16:02 -0300

    chore(errorHandler): remove unnecessary console.log

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,6 +1,5 @@
 
 const errorHandler= (err, req, res, next) => {
-    console.log('Error capturado por el middleware:', err);
      res.status(err.statusCode || 500).json({
         message: err.message || 'Error interno'
     });
