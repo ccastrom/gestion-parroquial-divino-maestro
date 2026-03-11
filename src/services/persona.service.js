958eb9e92c4947bac4b39493ed89a287112093db
ccatrom (comedor) <claudiocastrom7@gmail.com>
2026-03-11 16:33:57 -0300

    feat(tramite): add participant to tramite with transaction

--- src/services/persona.service.js
+++ src/services/persona.service.js
@@ -5,7 +5,22 @@ async function getPersonas(){
     return await Persona.findAll();
 }
 
+const getPersonById=async(id)=>{
+    
+        const persona= await Persona.findByPk(id);
+     if(!persona){
+            const error= new Error('Persona no encontrada');
+            error.statusCode=404;
+            throw error; 
+     }
+    
+     return persona;
+    
+    }
+    
+
 
 module.exports={
-    getPersonas
+    getPersonas,
+    getPersonById
 }
--- src/services/tramite.service.js
+++ src/services/tramite.service.js
@@ -1,6 +1,8 @@
+const sequelize = require('../config/database.js');
 const { Tramite, Participacion, Persona } = require('../models');
 const {ESTADOS_VALIDOS,validarEstado}= require('../constants/estados_tramites');
 const {ROLES_VALIDOS,validateRol}= require('../constants/roles_Participantes');
+const {getPersonById}=require('./persona.service');
 
 
 const cambiarEstadoTramite=async(id,nuevoEstado)=>{
@@ -14,8 +16,13 @@ const cambiarEstadoTramite=async(id,nuevoEstado)=>{
 const Test_tramiteParticipacion=async(id,datos)=>{
    const tramite= await getTramiteById(id);
    validateRol(datos.rol);
-   return tramite
-}
+   if(datos.personaId){
+     const persona= await getPersonById(datos.personaId);
+   }
+    await crearParticipante(id,datos);
+   return  tramite;
+  
+};
 
 const agregarParticipante=async(id,datos)=>{
 
@@ -45,13 +52,48 @@ const getTramites=async()=>{
     return await Tramite.findAll();
 }
 
+const crearParticipante = async(id, datos) => {
+    console.log('crearParticipante alcanzado', id, datos);
+    const transaction = await sequelize.transaction();
+    try {
+        if(datos.personaId){
+           await Participacion.create({
+                id_fk_tramite: id,
+                id_fk_persona: datos.personaId,
+                rol: datos.rol
+            }, { transaction });
+        } else {
+           const nuevaPersona = await Persona.create({
+                nombre: datos.persona.nombre,
+                apellido: datos.persona.apellido,
+                fecha_nacimiento: datos.persona.fecha_nacimiento,
+                rut: datos.persona.rut,
+                fono: datos.persona.fono,
+                direccion: datos.persona.direccion
+            }, { transaction });
+
+            await Participacion.create({
+                id_fk_tramite: id,
+                id_fk_persona: nuevaPersona.id,
+                rol: datos.rol
+            }, { transaction });
+        }
+        await transaction.commit();
+    } catch (error) {
+         console.log("Error en catch: ", error);
+        await transaction.rollback();
+        throw error;
+        
+    }
+};
+
 
 
 
 module.exports={
     createTramite,
     getTramites,
-   getTramiteById,
+    getTramiteById,
     cambiarEstadoTramite,
     agregarParticipante,
     Test_tramiteParticipacion
