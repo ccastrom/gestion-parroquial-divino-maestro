0158f66c17b97eadd6b0e7b6af8db5fd79837d64
ccatrom (comedor) <claudiocastrom7@gmail.com>
2026-03-13 14:52:24 -0300

    refactor(tramite): clean up Test_tramiteParticipacion and crearParticipante functions in service layer

--- src/services/tramite.service.js
+++ src/services/tramite.service.js
@@ -1,110 +1,133 @@
-const sequelize = require('../config/database.js');
-const { Tramite, Participacion, Persona } = require('../models');
-const {ESTADOS_VALIDOS,validarEstado}= require('../constants/estados_tramites');
-const {ROLES_VALIDOS,validateRol}= require('../constants/roles_Participantes');
-const {getPersonById}=require('./persona.service');
+const sequelize = require("../config/database.js");
+const { Tramite, Participacion, Persona } = require("../models");
+const {
+  ESTADOS_VALIDOS,
+  validarEstado,
+} = require("../constants/estados_tramites");
+const {
+  ROLES_VALIDOS,
+  validateRol,
+} = require("../constants/roles_Participantes");
+const { getPersonById } = require("./persona.service");
 
-
-const cambiarEstadoTramite=async(id,nuevoEstado)=>{
+const cambiarEstadoTramite = async (id, nuevoEstado) => {
   validarEstado(nuevoEstado);
-  const tramite= await getTramiteById(id);
-  tramite.estado=nuevoEstado;
+  const tramite = await getTramiteById(id);
+  tramite.estado = nuevoEstado;
   await tramite.save();
   return tramite;
-}
-
-const Test_tramiteParticipacion=async(id,datos)=>{
-   const tramite= await getTramiteById(id);
-   validateRol(datos.rol);
-   if(datos.personaId){
-     const persona= await getPersonById(datos.personaId);
-   }
-    await crearParticipante(id,datos);
-   return await getParticipantesByTramite(id);
 };
 
-const agregarParticipante=async(id,datos)=>{
+const Test_tramiteParticipacion = async (id, datos) => {
+  const participanteData = {
+    tramiteId: id,
+    personaId: datos.personaId,
+    persona: datos.persona,
+    rol: datos.rol,
+  };
+  const tramite = await getTramiteById(participanteData.tramiteId);
+  validateRol(participanteData.rol);
+  if (participanteData.personaId) {
+     await getPersonById(participanteData.personaId);
+  }
 
-    //EN ESPERA
-    const tramite= await getTramiteById(id);
-    return tramite;
-   
-
+  await crearParticipante(participanteData);
+  return await getParticipantesByTramite(participanteData.tramiteId);
 };
 
-const createTramite=async()=>{
-    return await Tramite.create({});
-}
+const agregarParticipante = async (id, datos) => {
+  //EN ESPERA
+  const tramite = await getTramiteById(id);
+  return tramite;
+};
 
-const getTramiteById=async(id)=>{
-    const tramite= await Tramite.findByPk(id);
-     if(!tramite){
-            const error= new Error('Trámite no encontrado');
-            error.statusCode=404;
-            throw error;
-    
+const createTramite = async () => {
+  return await Tramite.create({});
+};
+
+const getTramiteById = async (id) => {
+  const tramite = await Tramite.findByPk(id);
+  if (!tramite) {
+    const error = new Error("Trámite no encontrado");
+    error.statusCode = 404;
+    throw error;
   }
   return tramite;
-}
-
-const getTramites=async()=>{
-    return await Tramite.findAll();
-}
-
-const crearParticipante = async(id, datos) => {
-    console.log('crearParticipante alcanzado', id, datos);
-    const transaction = await sequelize.transaction();
-    try {
-        if(datos.personaId){
-           await Participacion.create({
-                id_fk_tramite: id,
-                id_fk_persona: datos.personaId,
-                rol: datos.rol
-            }, { transaction });
-        } else {
-           const nuevaPersona = await Persona.create({
-                nombre: datos.persona.nombre,
-                apellido: datos.persona.apellido,
-                fecha_nacimiento: datos.persona.fecha_nacimiento,
-                rut: datos.persona.rut,
-                fono: datos.persona.fono,
-                direccion: datos.persona.direccion
-            }, { transaction });
-
-            await Participacion.create({
-                id_fk_tramite: id,
-                id_fk_persona: nuevaPersona.id,
-                rol: datos.rol
-            }, { transaction });
-        }
-        await transaction.commit();
-    } catch (error) {
-         console.log("Error en catch: ", error);
-        await transaction.rollback();
-        throw error; 
-    }
-    await getParticipantesByTramite(id);
 };
 
-const getParticipantesByTramite= async(id)=>{
-    const participantes= await Participacion.findAll({
-        where:{id_fk_tramite:id},
-        include:[{
-            model:Persona,
-            attributes:['nombre','apellido','fecha_nacimiento','rut','fono','direccion']
-        }]
-    });
-    return participantes;
-}
+const getTramites = async () => {
+  return await Tramite.findAll();
+};
 
+const crearParticipante = async (participanteData) => {
 
+  const transaction = await sequelize.transaction();
+  try {
+    if (participanteData.personaId) {
+      await Participacion.create(
+        {
+          id_fk_tramite: participanteData.tramiteId,
+          id_fk_persona: participanteData.personaId,
+          rol: participanteData.rol,
+        },
+        { transaction },
+      );
+    } else {
+      const { nombre, apellido, fecha_nacimiento, rut, fono, direccion } =
+        participanteData.persona;
+      const nuevaPersona = await Persona.create(
+        {
+          nombre,
+          apellido,
+          fecha_nacimiento,
+          rut,
+          fono,
+          direccion,
+        },
+        { transaction },
+      );
 
-module.exports={
-    createTramite,
-    getTramites,
-    getTramiteById,
-    cambiarEstadoTramite,
-    agregarParticipante,
-    Test_tramiteParticipacion
-    
-}
+      await Participacion.create(
+        {
+          id_fk_tramite: participanteData.tramiteId,
+          id_fk_persona: nuevaPersona.id,
+          rol: participanteData.rol,
+        },
+        { transaction },
+      );
+    }
+    await transaction.commit();
+  } catch (error) {
+    await transaction.rollback();
+    throw error;
+  }
+};
+
+const getParticipantesByTramite = async (id) => {
+  const participantes = await Participacion.findAll({
+    where: { id_fk_tramite: id },
+    include: [
+      {
+        model: Persona,
+        attributes: [
+          "nombre",
+          "apellido",
+          "fecha_nacimiento",
+          "rut",
+          "fono",
+          "direccion",
+        ],
+      },
+    ],
+  });
+  return participantes;
+};
+
+module.exports = {
+  createTramite,
+  getTramites,
+  getTramiteById,
+  cambiarEstadoTramite,
+  agregarParticipante,
+  Test_tramiteParticipacion,
+};
