8d7d75b2d76c8af34878e2eac6154859d3c93ccc
ccatrom (comedor) <claudiocastrom7@gmail.com>
2026-03-17 12:52:11 -0300

    fix(reunion): remove required and allow null in reunion_prebautismal Joi schema

--- src/schemas/reunion_pre_bautizo.schema.js
+++ src/schemas/reunion_pre_bautizo.schema.js
@@ -2,8 +2,8 @@ const joi=require('joi');
 const {ESTADOS_VALIDOS}= require('../constants/estados_tramites');
 
 const actualizarReunionSchema = Joi.object({
-    fecha: Joi.date().required(),
-    id_fk_catequista: Joi.number().integer().positive().required(),
+    fecha: Joi.date().allow(null),
+    id_fk_persona: Joi.number().integer().positive().allow(null),
     estado: Joi.string().valid(...Object.values(ESTADOS_VALIDOS)).required()
 });
 
