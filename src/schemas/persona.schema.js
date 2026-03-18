ab7d8089c7970035416e1e4f09829b619fe9cb61
ccatrom (comedor) <claudiocastrom7@gmail.com>
2026-03-18 17:09:08 -0300

    feat(personas): make all fields optional except nombre and apellido in Joi schema

--- src/schemas/persona.schema.js
+++ src/schemas/persona.schema.js
@@ -4,10 +4,10 @@ const Joi = require('joi');
 const crearPersonaSchema = Joi.object({
     nombre: Joi.string().required(),
     apellido: Joi.string().required(),
-    fecha_nacimiento: Joi.date().required(),
+    fecha_nacimiento: Joi.date().allow(null,"").optional(),
     rut: Joi.string().allow(null,"").optional(),
-    fono: Joi.string().allow(null),
-    direccion: Joi.string().allow(null),  
+    fono: Joi.string().allow(null,"").optional(),
+    direccion: Joi.string().allow(null,"").optional(),  
 });
 
 const actualizarPersonaSchema = Joi.object({
