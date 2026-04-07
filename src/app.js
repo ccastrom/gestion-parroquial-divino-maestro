c5148abb9b5dc2703189901abd0711939c9cbfe9
ccastrom <claudiocastrom7@gmail.com>
2026-04-07 13:28:36 -0400

    fix(personas): validate content-type and prevent internal error exposure for POST request

--- src/app.js
+++ src/app.js
@@ -9,9 +9,14 @@ const errorHandler=require('./middleware/errorHandler');
 
 
 app.use(express.json());
+app.use((req, res, next) => {
+  if (req.method !== 'GET' && !req.is('application/json')) {
+    return res.status(400).json({ error: 'Content-Type debe ser application/json' });
+  }
+  next();
+});
 app.use('/personas',personasRoutes);
 app.use('/tramites',tramitesRoutes);
-
 app.use(errorHandler);
 module.exports=app;
 
